c05d27822045dc753a0b66d167e7620ee9bdffd8
Diego <131807665+dlop6@users.noreply.github.com>
2025-09-24 16:09:28 -0600

    feat: implement BMP texture loading and rendering, add basic ray tracing functionality

---
+++ Lab 7/Rasterizer2025.py
@@ -0,0 +1,93 @@
+import pygame
+from gl import *
+from BMP_Writer import GenerateBMP
+from model import Model
+from shaders import *
+
+width = 256
+height = 256
+
+screen = pygame.display.set_mode((width, height), pygame.SCALED)
+clock = pygame.time.Clock()
+
+rend = Renderer(screen)
+
+# triangle3 = [[510,70], [550, 160], [570,80] ]
+
+triangleModel = Model()
+triangleModel.vertices = [ 110,  70, 0,
+						   150, 160, 0,
+						   170,  80, 0 ]
+
+triangleModel.vertexShader = vertexShader
+
+rend.models.append(triangleModel)
+
+
+isRunning = True
+while isRunning:
+
+	deltaTime = clock.tick(60) / 1000.0
+
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			isRunning = False
+
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_1:
+				rend.primitiveType = POINTS
+
+			elif event.key == pygame.K_2:
+				rend.primitiveType = LINES
+
+			elif event.key == pygame.K_3:
+				rend.primitiveType = TRIANGLES
+
+
+
+	keys = pygame.key.get_pressed()
+
+	if keys[pygame.K_RIGHT]:
+		triangleModel.translation[0] += 10 * deltaTime
+	if keys[pygame.K_LEFT]:
+		triangleModel.translation[0] -= 10 * deltaTime
+	if keys[pygame.K_UP]:
+		triangleModel.translation[1] += 10 * deltaTime
+	if keys[pygame.K_DOWN]:
+		triangleModel.translation[1] -= 10 * deltaTime
+
+
+	if keys[pygame.K_d]:
+		triangleModel.rotation[2] += 20 * deltaTime
+	if keys[pygame.K_a]:
+		triangleModel.rotation[2] -= 20 * deltaTime
+
+	if keys[pygame.K_w]:
+		triangleModel.scale =  [(i + deltaTime) for i in triangleModel.scale]
+	if keys[pygame.K_s]:
+		triangleModel.scale = [(i - deltaTime) for i in triangleModel.scale ]
+
+
+
+
+
+
+
+
+
+
+	rend.glClear()
+
+	# Escribir lo que se va a dibujar aqui
+
+	rend.glRender()
+
+	#########################################
+
+	pygame.display.flip()
+
+
+GenerateBMP("output.bmp", width, height, 3, rend.frameBuffer)
+
+pygame.quit()
